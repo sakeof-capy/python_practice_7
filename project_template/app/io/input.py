74b29e9647922f785a01f396f9178e4393bc9870
Fourier2718281828 <non-linear@ukr.net>
2024-04-07 13:11:17 +0300

    added: io-functions empty definitions

---
+++ project_template/app/io/input.py
@@ -0,0 +1,8 @@
+def input_from_console():
+    pass
+def read_from_file(filename):
+    pass
+
+def read_from_file_with_pandas(filename):
+    pass
+
---
+++ project_template/app/io/output.py
@@ -0,0 +1,6 @@
+def output_to_console(text):
+    pass
+
+def write_to_file(filename, text):
+    pass
+
